49aaccb2b2dd2f4d72d47223183e528ee9dfdffb
dlinnyzhenia98-ctrl <dlinny.zhenia98@gmail.com>
2026-07-30 00:31:28 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // URL вашего Google Apps Script
-    API_URL: 'https://script.google.com/macros/s/AKfycbyDjZpslaA9VdCCFRDJk2Y_1dFl5fJKGFLDWbH9nSpq6mGfZTdvmzHv_YGy5huJsBR7MQ/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbyye02pNMnBE2pCRyaZaOVZroaduIXg9js6LYpKQcnM_o63Q3Vtvqe8PYjfzlMUoob2pg/exec',
     
     // Секретный ключ для API
     SECRET_KEY: 'qwertyhgfdsazxcvbn',
